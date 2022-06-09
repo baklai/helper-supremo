3cbeb2aa0dda38b460e88ecec1d038307ec720bd
Dmitrii <d.n.baklai@gmail.com>
2022-06-09 12:31:41 +0300

    edit project

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('public'));
-
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.get('/', function (req, res) {
