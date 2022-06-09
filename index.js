c905c613c0098793a15fba2cf997764f94f4c24b
Dmitrii <d.n.baklai@gmail.com>
2022-06-09 11:42:54 +0300

    edit path

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/node_modules'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 const PORT = process.env.PORT || 80;
