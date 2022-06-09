06d32c5e81ba5e420eb21fe72621398c42297e4f
Dmitrii <d.n.baklai@gmail.com>
2022-06-09 11:56:54 +0300

    edit path and port

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}...`);
